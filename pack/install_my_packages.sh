bf8daa926cc8d8df900672748864bd05cf5a6934
Maxim Kim <habamax@gmail.com>
2019-02-16 22:12:51 +0300

    my other packages bootstrap

---
+++ pack/install_my_packages.sh
@@ -0,0 +1,6 @@
+git clone https://github.com/habamax/vim-asciidoctor.git ./habamax/start/vim-asciidoctor
+git clone https://github.com/habamax/vim-evalvim.git ./habamax/start/vim-evalvim
+git clone https://github.com/habamax/vim-skipit.git ./habamax/start/vim-skipit
+git clone https://github.com/habamax/vim-elixir-mix-test.git ./habamax/start/vim-elixir-mix-test
+
+
