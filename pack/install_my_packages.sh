062617d26f5b6769e3e945b7215ab8bc7b036c29
Maxim Kim <habamax@gmail.com>
2019-02-28 12:37:22 +0300

    Move defminus and defnoche colors to a separate repos

--- pack/install_my_packages.sh
+++ pack/install_my_packages.sh
@@ -4,5 +4,8 @@ git clone https://github.com/habamax/vim-asciidoctor.git ./habamax/start/vim-asc
 git clone https://github.com/habamax/vim-evalvim.git ./habamax/start/vim-evalvim
 git clone https://github.com/habamax/vim-skipit.git ./habamax/start/vim-skipit
 git clone https://github.com/habamax/vim-elixir-mix-test.git ./habamax/start/vim-elixir-mix-test
+git clone https://github.com/habamax/vim-sendtoterm.git ./habamax/start/vim-sendtoterm
+git clone https://github.com/habamax/vim-colors-defminus.git ./habamax/start/vim-colors-defminus
+git clone https://github.com/habamax/vim-colors-defnoche.git ./habamax/start/vim-colors-defnoche
 
 
